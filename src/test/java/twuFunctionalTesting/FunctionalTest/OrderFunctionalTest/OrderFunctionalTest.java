bb6405ff82c6ccc314ee8c191e053bdbfbb82a6f
mmlewis <mlewis@thoughtworks.com>
2012-07-26 13:33:06 -0500

    updated assertions

--- src/test/java/twuFunctionalTesting/FunctionalTest/OrderFunctionalTest/OrderFunctionalTest.java
+++ src/test/java/twuFunctionalTesting/FunctionalTest/OrderFunctionalTest/OrderFunctionalTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 public class OrderFunctionalTest {
 
@@ -16,19 +16,18 @@ public class OrderFunctionalTest {
     //on  new form submit check the show form is displayed
     @Test
     public void SubmitForm(){
-
         WebDriver driver = new FirefoxDriver();
         driver.get("http://localhost:8080/twuFunctionalTesting/order/new");
         WebElement name = driver.findElement(By.name("name"));
         name.sendKeys("Meaghan");
         WebElement email = driver.findElement(By.name("email"));
         email.sendKeys("mlewis@thoughtworks.com");
-        WebElement item = driver.findElement(By.id("items"));
-        item.sendKeys("To Kill A Mockingbird");
         WebElement submit = driver.findElement(By.name("submitButton"));
         submit.submit();
         driver.close();
 
+        assertNotNull(name);
+        assertNotNull(email);
         assertNotSame(name,email);
     }
 
